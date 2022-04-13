27239a5ee6e69e2361ac7d3da2a02dd14b7a28ea
Huraira <muhammadhuraira4@gmail.com>
2022-04-13 19:54:59 +0500

    Some more changings

--- q1/lib/QuestionBank.dart
+++ q1/lib/QuestionBank.dart
@@ -4,16 +4,16 @@ class QuestionBank {
   int _questionNo = 0;
 
   final List<Question> questionList = [
-    Question("Just a check question \n 1 + 1 = 2", true),
-    Question("Q2", false),
-    Question("Q3", false),
-    Question("Q4", false),
-    Question("Q5", true),
-    Question("Q6", true),
-    Question("Q7", false),
-    Question("Q8", true),
-    Question("Q9", true),
-    Question("Q10", false),
+    Question("1 + 1 = 2", true),
+    Question("Apple is a vegetable", false),
+    Question("Sun is smaller than moon", false),
+    Question("Lahore is Capital of Pakistan", false),
+    Question("Computer understands Binary language", true),
+    Question("All human are mortals", true),
+    Question("Isaac Newton Developed the Theory of General Relativity", false),
+    Question("Physics is a branch of science", true),
+    Question("Atoms are smaller than Molecules", true),
+    Question("Iron is not a metal", false),
   ];
   void NextQuestion() {
     if (_questionNo <= questionList.length - 1) {
